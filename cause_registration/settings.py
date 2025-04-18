3df9c57a8529bfb2e4eb23ad33f972a83dd5a184
MistyDragon7 <120657456+MistyDragon7@users.noreply.github.com>
2025-04-18 11:20:12 +0530

    Basic Setup for Django

--- cause_registration/settings.py
+++ cause_registration/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b(o%%52(e+cxqzlcgf0ff#owtc8_zcug!gx$=$hy)vo8793#qe'
+SECRET_KEY = 'cha-ching'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '.now.sh']
 
 
 TWILIO_ACCOUNT_SID = 'AC068a8175b74b797837b6c324d44401bc'
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
